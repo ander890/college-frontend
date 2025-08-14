33a6d01e1b17753b6f4f6013d04ad7778e218a46
Daniel Alexander <38150405+ander890@users.noreply.github.com>
2025-08-14 15:38:39 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import type { Configuration } from 'webpack';
 
-
-const nextConfig = {
+const nextConfig: NextConfig = {
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || 'https://college.youthmultiply.com',
-  webpack(config) {
-    config.module.rules.push({
+  webpack(config: Configuration) {
+    config.module?.rules?.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
     return config;
   },
 };
-export default nextConfig;
 
+export default nextConfig;
